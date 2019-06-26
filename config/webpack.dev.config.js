d8af0ffa517635a43c77a2f20c5440d0d2e86dd6
wangj3 <1759714040@qq.com>
2019-06-26 17:25:03 +0800

    test

---
+++ config/webpack.dev.config.js
@@ -0,0 +1,11 @@
+const merge = require('webpack-merge')
+const baseConfig = require('./webpack.base.config');
+
+module.exports = merge(baseConfig,{
+  mode:'development',
+  devtool:'inline-source-map',
+  devServer: {
+    contentBase:'./dist',
+    port:3001
+  }
+})
